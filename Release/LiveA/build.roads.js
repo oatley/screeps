8e9cc9d1db2028e4075dd784bfcf6297930b4b9b
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-04-24 21:03:52 -0400

    Debug2

--- Release/LiveA/build.roads.js
+++ Release/LiveA/build.roads.js
@@ -137,7 +137,7 @@ var buildRoads = {
                 pathToSpawn = extensionToUse.memory.pathToSpawn;
             }
 
-            console.log(room.memory.extensions[extensionToUse.id].createdPath);
+            console.log(extensionToUse.id, room.memory.extensions[extensionToUse.id].createdPath);
             //return;
             //room.memory.extensions[extensionToUse.id].createdPath = false;
             if(!room.memory.extensions[extensionToUse.id].createdPath || forceRebuild) {
