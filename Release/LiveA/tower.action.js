31641d536270dc72113ba0c5e063e0b4dc34b3b1
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-04-24 22:33:10 -0400

    closestHostile

--- Release/LiveA/tower.action.js
+++ Release/LiveA/tower.action.js
@@ -7,20 +7,16 @@ var towerAction = {
         });
         for (var tower in towerTargets) {
             if(tower) {
-                var closestHostile = towerTargets[tower].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-                if(closestHostile) {
-                    towerTargets[tower].attack(closestHostile);
-                }
                 //console.log(towerTargets[tower]);
                 var closestDamagedStructure = towerTargets[tower].pos.findClosestByRange(FIND_STRUCTURES, {
                     filter: (structure) =>  (structure.hits < structure.hitsMax && structure.structureType == STRUCTURE_ROAD) ||
                                             (structure.hits < 10000 &&  structure.structureType == STRUCTURE_RAMPART) ||
                                             (structure.hits < 100000 &&  structure.structureType == STRUCTURE_WALL)
                 });
-                if(closestDamagedStructure) {
+                var closestHostile = towerTargets[tower].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                if(closestDamagedStructure && !closestHostile) {
                     let repair = towerTargets[tower].repair(closestDamagedStructure);
                 }
-                var closestHostile = towerTargets[tower].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
                 if(closestHostile) {
                     towerTargets[tower].attack(closestHostile);
                 }
