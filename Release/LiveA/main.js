50145aee7cd77bdf607873529c1ff8617324485c
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-04-25 12:32:58 -0400

    s

--- Release/LiveA/main.js
+++ Release/LiveA/main.js
@@ -10,7 +10,7 @@ var towerAction = require('tower.action');
 
 module.exports.loop = function () {
 
-    // Testing githook psuh pushccsssssssssssssssssss
+    // Testing githook psuh pushccssssssssssssssssssss
     //if (spawn.room.energyCapacityAvailable >)
     Memory.data = {maxCreeps: 8, bodyParts: 3};
     cleanMemory.clean(); // Clean dead creeps from memory
