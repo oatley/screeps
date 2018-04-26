0375ead53df4fa0935939924b70c380534c9e379
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-04-26 11:33:06 -0400

    Detect exit tiles

--- Release/LiveA/build.walls.js
+++ Release/LiveA/build.walls.js
@@ -2,7 +2,20 @@ var buildWalls = {
     new: function(room) {
         let exits = Game.map.describeExits(room);
         for (let exit in exits) {
+            //FIND_EXIT_TOP: 1,
+            //FIND_EXIT_RIGHT: 3,
+            //FIND_EXIT_BOTTOM: 5,
+            //FIND_EXIT_LEFT: 7,
             console.log(exit, exits[exit]);
+            if (exit == FIND_EXIT_RIGHT) {
+                let x = 49;
+                for (let y in _.range(0,50)) {}
+                    let terrain = Game.map.getTerrainAt(x, y, room.name);
+                    if (terrain == 'plain') {
+                        console.log('[build.walls] - Found exit', terrain, x, y);
+                    }
+                }
+            }
         }
     }
 };
