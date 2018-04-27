af6f50768acfc8bdf06f99e19c0db94d9a4be0fa
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-04-26 21:29:56 -0400

    Towers repair ramparts and roads

--- Release/LiveA/tower.action.js
+++ Release/LiveA/tower.action.js
@@ -7,17 +7,17 @@ var towerAction = {
         });
         for (var tower in towerTargets) {
             if(tower) {
-                var closestDamagedRampart = towerTargets[tower].pos.findClosestByRange(FIND_STRUCTURES, {
-                    filter: (structure) =>  (structure.hits < 5000 &&  structure.structureType == STRUCTURE_RAMPART) });
+                //var closestDamagedRampart = towerTargets[tower].pos.findClosestByRange(FIND_STRUCTURES, {
+                //    filter: (structure) =>  (structure.hits < 5000 &&  structure.structureType == STRUCTURE_RAMPART) });
                 //console.log(towerTargets[tower]);
                 var closestDamagedStructure = towerTargets[tower].pos.findClosestByRange(FIND_STRUCTURES, {
                     filter: (structure) =>  (structure.hits < structure.hitsMax && structure.structureType == STRUCTURE_ROAD) ||
-                                            (structure.hits < 100000 &&  structure.structureType == STRUCTURE_RAMPART) ||
-                                            (structure.hits < 100000 &&  structure.structureType == STRUCTURE_WALL)
+                                            (structure.hits < 100000 &&  structure.structureType == STRUCTURE_RAMPART) //||
+                                            //(structure.hits < 100000 &&  structure.structureType == STRUCTURE_WALL)
                 });
                 var closestHostile = towerTargets[tower].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
                 if(closestDamagedRampart && !closestHostile) {
-                    let repair = towerTargets[tower].repair(closestDamagedRampart);
+                    let repair = towerTargets[tower].repair(closestDamagedStructure);
                 }
                 if(closestHostile) {
                     towerTargets[tower].attack(closestHostile);
