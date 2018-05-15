b3e09ea7203afb3137fa8088d09e22edabc7bb1f
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-05-14 22:09:53 -0400

    Force creeps to be counted by room

--- Release/LiveA/spawn.creeper.js
+++ Release/LiveA/spawn.creeper.js
@@ -6,10 +6,10 @@ var spawnCreeper = {
         var idnum = Math.round(Math.random() * 1000).toString();
         var allCreepsInGame = _.filter(Game.creeps, (creep) => true);
         let allCreeps = spawn.room.find (FIND_MY_CREEPS);
-        var allExplorers = _.filter(Game.creeps, (creep) => creep.memory.role == 'explorer');
-        var allBuilders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-        var allWorkers = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker');
-        var allUpgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+        var allExplorers = spawn.room.find (FIND_MY_CREEPS, (creep) => creep.memory.role == 'explorer');
+        var allBuilders = spawn.room.find (FIND_MY_CREEPS, (creep) => creep.memory.role == 'builder');
+        var allWorkers = spawn.room.find (FIND_MY_CREEPS, (creep) => creep.memory.role == 'worker');
+        var allUpgraders = spawn.room.find (FIND_MY_CREEPS, (creep) => creep.memory.role == 'upgrader');
 
 
         // Do not spawn if too many creeps
