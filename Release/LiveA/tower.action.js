5921475051835f567080f28d52a6df99985cf150
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-05-14 22:25:51 -0400

    Tower attack plz

--- Release/LiveA/tower.action.js
+++ Release/LiveA/tower.action.js
@@ -16,6 +16,11 @@ var towerAction = {
                                             //(structure.hits < 100000 &&  structure.structureType == STRUCTURE_WALL)
                 });
                 var closestHostile = towerTargets[tower].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                console.log('[tower.action] - ', closestHostile);
+                if(closestHostile) {
+                    console.log('[tower.action] - Enemy detected', closestHostile);
+                    towerTargets[tower].attack(closestHostile);
+                }
                 if(closestDamagedStructure && !closestHostile) {
                     if (closestDamagedRampart > 0){
                         let repair = towerTargets[tower].repair(closestDamagedRampart);
