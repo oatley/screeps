b66ea37a4459f4d6d961391bc9ecb2041e0d6d75
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-05-20 12:45:07 -0400

    FIxed bugs with rooms

--- Release/LiveA/main.js
+++ Release/LiveA/main.js
@@ -101,7 +101,7 @@ module.exports.loop = function () {
             ownedRooms.push(room);
         }
         // If creeps enter a room you don't own, don't try and build in it
-        if (!Game.rooms[room].controller.owner || spawnTargets.length < 1) {
+        if (((Game.rooms[i].controller) && (Game.rooms[i].controller.owner) && (Game.rooms[i].controller.owner.username != 'oatley')) || spawnTargets.length < 1) {
             continue;
         }
 
@@ -196,7 +196,6 @@ module.exports.loop = function () {
             let spawnTargets = Game.rooms[i].find(FIND_STRUCTURES, {
                         filter: (structure) => {return (structure.structureType == STRUCTURE_SPAWN);}
             });
-            console.log(Game.rooms[i].controller, Game.rooms[i].controller.owner)
             if (spawnTargets.length > 0 && (Game.rooms[i].controller) && (Game.rooms[i].controller.owner) && (Game.rooms[i].controller.owner.username == 'oatley')) {
                 let index = Memory.data.expandRooms.indexOf(Game.rooms[i].name);
                 if (index > -1) {
